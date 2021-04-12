9e07f2fa69d91386a56831a32f89cbf077ca50db
Felix Tham <ftham@ucsc.edu>
2021-04-12 05:30:33 -0700

    enemy ships wrapping around

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -3,14 +3,37 @@ class Rocket extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture, frame)
         scene.add.existing(this);
         this.movementSpeed = 2;
+        this.isFiring = false;
     }
 
     update() {
-        if(keyLEFT.isDown) {
-            this.x -= 2;
+        if(this.isFiring) {
+            this.y -= this.movementSpeed;
+            if(this.y < borderUISize*3) {
+                this.reset();
+            }
+        } else {
+            if(keyLEFT.isDown) {
+                this.x -= this.movementSpeed;
+            }
+            if(keyRIGHT.isDown) {
+                this.x += this.movementSpeed;
+            }
+
+            if(Phaser.Input.Keyboard.JustDown(keyF)) {
+                this.isFiring = true;
+            }
+    
+            this.x = Phaser.Math.Clamp(
+                this.x,
+                borderUISize + borderPadding,
+                game.config.width - borderUISize - borderPadding);
         }
-        if(keyRIGHT.isDown) {
-            this.x += 2;
-        }
+        
+    }
+
+    reset() {
+        this.y = game.config.height-borderUISize-borderPadding;
+        this.isFiring = false;
     }
 }
