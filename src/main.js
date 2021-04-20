4c36606d181117441749792ec6f82ad9ebb183e4
Felix Tham <ftham@ucsc.edu>
2021-04-20 10:19:06 -0700

    mod asset

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 let config = {
     type: Phaser.CANVAS,
-    width: 640,
+    width: 1280,
     height: 480,
     scene: [Menu, Play],
 }
@@ -10,4 +10,4 @@ let game = new Phaser.Game(config);
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
-let keyLEFT, keyRIGHT, keyF, keyR;
+let keyLEFT, keyRIGHT, keyF, keyR, keyW, keyA, keyD, keyUP, keyDOWN;
